964389724f5e27724e0045169f1933ab6eabebac
Alisa Jin <alisajin@Alisas-MacBook-Pro-2.local>
2023-09-26 11:13:18 -0700

    Setup webpack for application

---
+++ src/components/App.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function App() {
+  return (
+    <div>Hellooooooooo</div>
+  )
+}
+
+export default App
---
+++ webpack.config.js
@@ -0,0 +1,52 @@
+const path = require('path');
+const HTMLWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    entry: './src/index.js',
+
+    output: {
+        path: path.join(__dirname, '/dist'),
+        filename: 'bundle.js'
+    },
+
+    plugins: [
+        new HTMLWebpackPlugin({
+            template: './src/index.html'
+        })
+    ],
+
+    devServer: {
+        static: {
+            directory: path.resolve(__dirname, 'dist'),
+            publicPath: '/'
+        },
+        proxy: {
+            '/': 'http://localhost:3000/'
+        }
+    },
+    module: {
+        rules: [
+            {
+                test: /\.jsx?/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env', '@babel/preset-react']
+                    }
+                }
+            },
+            {
+                test: /.css$/,
+                exclude: /node_modules/,
+                use: ['style-loader', 'css-loader']
+            },
+        ]
+    },
+
+    resolve: {
+        extensions: ['.js', '.jsx']
+    }
+
+
+}
